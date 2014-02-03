34ba8ff0e637192226a1b2765769778fdb7a4a3b
arjunan <praveenvignesh31@gmail.com>
2014-02-03 13:32:25 +0530

    intial commit

---
+++ topmenu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+
+<script src="js/jquery-1.8.0.js" type="text/javascript"></script> 
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="shortcut icon" href="img/favicon1.png" />
+
+</head>
+
+<body>
+<div class="menuback">
+
+<div id="title"></div>
+
+<div id="container">
+  <ul class="nav nav-pills pull-right">
+    <li class="home" id="home"><a href="index.php">Home</a></li>
+    <li class="xprez" id="xprez"><a href="xissue.php">Xprez</a></li>
+    <li class="thiran" id="thiran"><a href="tissue.php">Thiran<img src="img/new.gif" /></a></li>
+    <li class="sangamam" id="sangamam"><a href="sissue.php">Sangamam</a></li>
+    <li class="special" id="special"><a href="special.php">Special</a></li>
+    <li class="about" id="about"><a href="about.php">About</a></li>
+  </ul>
+</div>
+
+<div id="border"></div>
+
+</div>
+
+</body>
+</html>
